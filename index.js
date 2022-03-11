d83e78cedcb0d638d111e138c05a83c256b3e9d5
RianOP <87343593+RianOP@users.noreply.github.com>
2022-03-10 19:08:14 -0500

    Made Base

---
+++ index.js
@@ -0,0 +1,14 @@
+"use strict";
+
+console.log(chalk.green("[Faliactyl] Loading Files..."));
+const antivpn = require("./handlers/antivpn")
+const config = require("./config.json");
+console.log(chalk.green("[Faliactyl] Files loaded"));
+
+    console.log(chalk.green("----------------------------------------------------"));
+    console.log(chalk.green("PterodactylDash"));
+    console.log(chalk.green("Release: Beta v1.0.0"));
+    console.log(chalk.green("----------------------------------------------------"));
+    console.log(chalk.green("PterodactylDash is running on port " + config.port));
+    console.log(chalk.green("----------------------------------------------------"));
+  });
