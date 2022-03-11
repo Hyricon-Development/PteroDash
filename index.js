63adc23abfd338b382275a1c43951717827e2d0a
RianOP <87343593+RianOP@users.noreply.github.com>
2022-03-11 16:52:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
-console.log(chalk.green("[Faliactyl] Loading Files..."));
 const config = require("./config.json");
 console.log(chalk.green("[Faliactyl] Files loaded"));
+
+let routes = fs.readdirSync('./routes').filter(file => file.endsWith('.js'));
+routes.forEach(file => {
+let routes = require(`./routes/${file}`);
+routes.load(app, db);
+console.log(chalk.green("[Faliactyl] Routes loaded"));
+
+let handlers = fs.readdirSync('./handlers').filter(file => file.endsWith('.js'));
+handlers.forEach(file => {
+let handlers = require(`./handlers/${file}`);
+handlers.load(app, db);    
+console.log(chalk.green("[Faliactyl] Handlers loaded"));
 
     console.log(chalk.green("----------------------------------------------------"));
     console.log(chalk.green("PterodactylDash"));
