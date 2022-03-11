006f1643a7972912e2f81c9ac4fa37e117855aea
RianOP <87343593+RianOP@users.noreply.github.com>
2022-03-11 16:53:50 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,16 +6,18 @@ routes.forEach(file => {
 let routes = require(`./routes/${file}`);
 routes.load(app, db);
 console.log(chalk.green("[Faliactyl] Routes loaded"));
+});
 
 let handlers = fs.readdirSync('./handlers').filter(file => file.endsWith('.js'));
 handlers.forEach(file => {
 let handlers = require(`./handlers/${file}`);
 handlers.load(app, db);    
 console.log(chalk.green("[Faliactyl] Handlers loaded"));
+});
 
-    console.log(chalk.green("----------------------------------------------------"));
-    console.log(chalk.green("PterodactylDash"));
-    console.log(chalk.green("Release: Beta v1.0.0"));
-    console.log(chalk.green("----------------------------------------------------"));
-    console.log(chalk.green("PterodactylDash is running on port " + config.port));
-    console.log(chalk.green("----------------------------------------------------"));
+console.log(chalk.green("----------------------------------------------------"));
+console.log(chalk.green("PterodactylDash"));
+console.log(chalk.green("Release: Beta v1.0.0"));
+console.log(chalk.green("----------------------------------------------------"));
+console.log(chalk.green("PterodactylDash is running on port " + config.port));
+console.log(chalk.green("----------------------------------------------------"));
