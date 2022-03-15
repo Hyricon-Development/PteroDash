44d45e0b3c9029e8b91a1476ea0c551d0cfae02c
RianOP <87343593+RianOP@users.noreply.github.com>
2022-03-14 20:45:28 -0400

    Update mysql.js

--- handlers/mysql.js
+++ handlers/mysql.js
@@ -1,10 +1,7 @@
 const fs = require("fs");
 const Keyv = require('keyv');
-const config = require('../config.json')
-module.exports.renderdataeval =
-  `(async () => {
-   let config = JSON.parse(require("fs").readFileSync("../config.json"));
-})();`;
+const config = require('../sync').syncconfig().config;
+
 const database = new Keyv('mysql://config.database.mysql.username:config.database.mysql.password@config.database.mysql.host/config.database.mysql.database');
 
 module.exports = {
