bb30747a45bd533c11486d287b1cf0265aaf6f95
RianOP <87343593+RianOP@users.noreply.github.com>
2022-03-14 20:42:35 -0400

    Create sync.js

---
+++ handlers/sync.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+var jsonsync = require('json');
+
+module.exports.renderdataeval = {
+syncconfig: function syncconfig() {
+   let config = JSON.parse(require("fs").readFileSync("../config.json"));
+   let json = jsonsync.toJson(data)
+   return JSON.jsonsync(json);
+}
+}
