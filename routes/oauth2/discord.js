274d19533695c88ec2a6f72be92fca82790fc9b7
RianOP <87343593+RianOP@users.noreply.github.com>
2022-03-18 20:01:53 -0400

    Update discord.js

--- routes/oauth2/discord.js
+++ routes/oauth2/discord.js
@@ -5,7 +5,7 @@ const suspendCheck = require("../servers/suspension_system.js");
 const express = require("express");
 module.exports.load = async function (app) {
   app.get("/oauth2/discord", async (req, res) => {
-    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${config.discord.oauth2.id}&redirect_uri=${config.discord.oauth2.redirect}&response_type=code&scope=identify%20email${config.discord.bot.joinguild.enabled == true ? "%20guilds.join" : ""}${newsettings.api.client.j4r.enabled == true ? "%20guilds" : ""}${settings.api.client.oauth2.prompt == false ? "&prompt=none" : (req.query.prompt ? (req.query.prompt == "none" ? "&prompt=none" : "") : "")}`
+    res.redirect(`https://discord.com/api/oauth2/authorize?client_id=${config.discord.oauth2.id}&redirect_uri=${config.discord.oauth2.redirect}&response_type=code&scope=identify%20email${config.discord.bot.joinguild.enabled == true ? "%20guilds.join" : ""}${config.j4r.enabled == true ? "%20guilds&prompt=none" : (req.query.prompt ? (req.query.prompt == "none" ? "&prompt=none" : "") : "")}`
     );
   });
 }
