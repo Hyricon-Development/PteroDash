cc11470d5ab4c77648f1cfa91947ac367cc53613
RianOP <87343593+RianOP@users.noreply.github.com>
2022-03-31 21:13:36 -0400

    Update bot.js

--- handlers/bot.js
+++ handlers/bot.js
@@ -5,6 +5,7 @@ const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
 const config = require('../handlers/sync').syncconfig().config;
 const database = require('../handlers/databases').getdatabase().database
 
+if (config.booster.enabled == true) {
 bot.on('guildMemberUpdate', async (oldMember, newMember) => {
     const role = oldMember.roles.find(role => role.id === config.booster.roleid);
     const role2 = newMember.roles.find(role => role.id === config.booster.roleid);
@@ -14,7 +15,8 @@ bot.on('guildMemberUpdate', async (oldMember, newMember) => {
 	let coins = (await database.get(`${useremail}/coins`)) ? (await database.get(`${useremail}/coins`)) : 0;
     let coins2 = coins + parseInt(config.booster.coins)
     await database.set(`${useremail}/coins`, coins2)
-}
+  }
 
 client.login(config.discord.bot.token);
-});
+ });
+}
