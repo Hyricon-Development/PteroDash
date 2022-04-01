114b9d716b99fc927db24ddb04f33dbff27ac60a
Rian_ <87343593+RianOP@users.noreply.github.com>
2022-04-01 17:29:43 -0400

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -1,11 +1,8 @@
 #!/bin/bash
 
-directory=$(pwd)
-if [[ "$directory" != *"PterodactylDash" && "$directory" != *"pterodactyldash" ]]; then
-  echo "Please run this script from the main PterodactylDash directory."
-  exit 1
-fi
-mv config.json config.json.backup
-git pull
+mv /var/www/PterodactylDash /var/www/PterodactylDash/Backup
+cd /var/www
+git clone https://github.com/Evolution-Development/PterodactylDash.git
 npm install
+cp /var/www/PterodactylDash/Backup/config.json /var/www/PterodactylDash/config.json.backup
 echo "You will have to merge your config.json manually. They have been backed up as config.json.backup"
