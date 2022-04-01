3ea7ccc03c2f3c7ee740b0a5f90b1297774adc5e
Rian_ <87343593+RianOP@users.noreply.github.com>
2022-04-01 17:30:03 -0400

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -3,6 +3,7 @@
 mv /var/www/PterodactylDash /var/www/PterodactylDash/Backup
 cd /var/www
 git clone https://github.com/Evolution-Development/PterodactylDash.git
+cd PterodactylDash
 npm install
 cp /var/www/PterodactylDash/Backup/config.json /var/www/PterodactylDash/config.json.backup
 echo "You will have to merge your config.json manually. They have been backed up as config.json.backup"
